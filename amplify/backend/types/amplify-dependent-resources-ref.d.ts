b50bf6597f7065e4d004e2ad726ffc104b965338
njherg <72587642+njherg@users.noreply.github.com>
2022-08-08 22:37:36 -0500

    Adicionar API y Storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s38a3e4396": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
